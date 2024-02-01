7d80af4cab66f75f43e9646ff13e460db1d6bb68
Nathan <natidare61@gmail.com>
2024-02-01 19:13:55 +0300

    Refactored and updated frontend code

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ import Card from "./Card";
 const Hero = ({ image, title, desc, date }) => {
   useEffect(() => {
     AOS.init();
-  });
+  }, []);
 
   return (
     <>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,24 @@
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="flex justify-between bg-slate-800 py-6">
-      <h2 className="text-white text-3xl font-serif font ml-6">Cosmic Lens</h2>
+      <h2 className="text-white text-3xl font-serif font ml-6">
+        <Link to="/">Cosmic Lens</Link>
+      </h2>
       <div>
         <ul className="flex justify-start gap-5 text-xl text-white font-mono mr-12 ">
-          <li className="hover:text-cyan-400 hover:cursor-pointer ">Home</li>
-          <li className="hover:text-cyan-400  hover:cursor-pointer ">
-            Services
+          <li className="hover:text-cyan-400 hover:cursor-pointer ">
+            <Link to="/">Home</Link>
           </li>
-          <li className="hover:text-cyan-400 hover:cursor-pointer ">Contact</li>
-          <li className="hover:text-cyan-400 hover:cursor-pointer ">About</li>
+          <li className="hover:text-cyan-400  hover:cursor-pointer ">
+            <Link to="/services">Services</Link>
+          </li>
+          <li className="hover:text-cyan-400 hover:cursor-pointer ">
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li className="hover:text-cyan-400 hover:cursor-pointer ">
+            <Link to="/about">About</Link>
+          </li>
         </ul>
       </div>
     </div>
